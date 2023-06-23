7e07a6ac55c792291e15b002c03333820a5d0741
Logan Volkers <loganvolkers@users.noreply.github.com>
2023-06-23 22:34:54 +0000

    Setup CodeSandbox, vanilla vue3 example

---
+++ example-vue3/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useAtom, atom } from "jotai-vue";
+
+const countAtom = atom(3);
+
+const [count, setCount] = useAtom(countAtom);
+</script>
+
+<template>
+  <div>
+    <h1>{{ count }}</h1>
+    <button @click="setCount((prev) => prev + 1)">One up</button>
+  </div>
+</template>
