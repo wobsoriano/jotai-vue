b09049f0c84056a79d560c8a82639c0012b1aec5
Logan Volkers <loganvolkers@users.noreply.github.com>
2023-06-24 01:39:36 +0000

    Follow Vue recommendations on refs

--- src/useAtomValue.ts
+++ src/useAtomValue.ts
@@ -1,54 +1,60 @@
-import type { Atom, ExtractAtomValue } from 'jotai/vanilla'
-import { getCurrentInstance, onScopeDispose, readonly, ref } from 'vue'
-import { useStore } from './Provider'
-import type { AwaitedRef } from './useAtom'
+import type { Atom, ExtractAtomValue } from "jotai/vanilla";
+import { getCurrentInstance, onScopeDispose, readonly, shallowRef, ref } from "vue";
+import { useStore } from "./Provider";
+import type { AwaitedRef } from "./useAtom";
 
-type Store = ReturnType<typeof useStore>
+type Store = ReturnType<typeof useStore>;
 
-const isPromise = (x: unknown): x is Promise<unknown> => x instanceof Promise
+const isPromise = (x: unknown): x is Promise<unknown> => x instanceof Promise;
 
 interface Options {
-  store?: Store
+  store?: Store;
   /**
    * @internal
    */
-  storage?: typeof ref
+  storage?: typeof ref;
   /**
    * @internal
    */
-  storageKey?: string
+  storageKey?: string;
 }
 
 export function useAtomValue<Value>(
   atom: Atom<Value>,
   options?: Options
-): AwaitedRef<Value>
+): AwaitedRef<Value>;
 
 export function useAtomValue<AtomType extends Atom<unknown>>(
   atom: AtomType,
   options?: Options
-): AwaitedRef<ExtractAtomValue<AtomType>>
+): AwaitedRef<ExtractAtomValue<AtomType>>;
 
 export function useAtomValue<Value>(atom: Atom<Value>, options?: Options) {
-  const store = useStore({ store: options?.store })
-  const initialValue = store.get(atom)
+  const store = useStore({ store: options?.store });
+  const initialValue = store.get(atom);
 
   if (isPromise(initialValue))
-    throw new Error('[jotai-vue]: Async atom values are not supported.')
+    throw new Error("[jotai-vue]: Async atom values are not supported.");
 
-  // @ts-expect-error: For use with Nuxt useState
-  const atomValue = options?.storage ? options.storage(options.storageKey, () => initialValue) : ref(initialValue)
+  const atomValue = options?.storage
+    ? options.storage(options.storageKey, () => initialValue)
+    /**
+     * Prefer `shallowRef` over `ref` for external state
+     * - https://vuejs.org/api/reactivity-advanced.html#shallowref
+     * -
+     */ 
+    : shallowRef(initialValue);
 
   const unsub = store.sub(atom, () => {
-    const nextValue = store.get(atom)
-    atomValue.value = nextValue as any
-  })
+    const nextValue = store.get(atom);
+    atomValue.value = nextValue as any;
+  });
 
   if (getCurrentInstance()) {
     onScopeDispose(() => {
-      unsub()
-    })
+      unsub();
+    });
   }
 
-  return readonly(atomValue)
+  return readonly(atomValue);
 }
